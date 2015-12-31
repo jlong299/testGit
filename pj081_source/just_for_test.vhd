04a3ef09f2a6389fc2c789c3662e844a02ecf2c3
jlong299 <jlong299@foxmail.com>
2015-12-31 11:29:36 +0800

    test2

--- pj081_source/just_for_test.vhd
+++ pj081_source/just_for_test.vhd
@@ -15,17 +15,7 @@ architecture rtl of PN_gen4_ena is
 signal a : std_logic_vector(22 downto 0);
 
 begin
-process(aReset,clk)
-begin
-	if aReset = '1' then
-		a <= "01000000000000000000000";
-		PN_Dataout <= (others => '0');
-	elsif rising_edge(clk) then
-	if ena = '1' then
-		a(22 downto 19) <= a(3 downto 0) XOR a(8 downto 5); --a(8 downto 7);
-		a(18 downto 0) <= a(22 downto 4);
-		PN_Dataout <= a(3 downto 0);
-	end if;
-	end if;
-end process;
+
+
+
 end rtl;
